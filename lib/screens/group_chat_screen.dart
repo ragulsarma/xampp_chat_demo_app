c0c191d1235968cc6de686b61f48d65650b461ff
ragulnathan <ragulsarma@gmail.com>
2024-03-07 23:08:44 +0530

    group chat page integration completed

--- lib/screens/group_chat_screen.dart
+++ lib/screens/group_chat_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_xmpp_login/screens/login_screen.dart';
+import 'package:xmpp_chat_demo_flutter/screens/login_screen.dart';
 import 'package:xmpp_plugin/error_response_event.dart';
 import 'package:xmpp_plugin/models/chat_state_model.dart';
 import 'package:xmpp_plugin/models/connection_event.dart';
@@ -13,7 +13,7 @@ import 'package:xmpp_plugin/xmpp_plugin.dart';
 class GroupChatScreen extends StatefulWidget {
   final String groupName;
 
-  const GroupChatScreen({Key? key, required this.groupName}) : super(key: key);
+  const GroupChatScreen({super.key, required this.groupName});
 
   @override
   State<GroupChatScreen> createState() => _GroupChatScreenState();
@@ -28,17 +28,14 @@ class _GroupChatScreenState extends State<GroupChatScreen>
   final TextEditingController _textFieldController = TextEditingController();
   String valueText = '';
 
-/*  void sendMsg() async {
+  void sendMsg() async {
     if (_msgController.text.isNotEmpty) {
       int id = DateTime.now().millisecondsSinceEpoch;
 
       final groupName = widget.groupName.replaceAll(" ", "");
 
-      await LoginScreenState.flutterXmpp.sendGroupMessage(
-          '$groupName@conference.mongoose.netaxis.co',
-          _msgController.text,
-          "$id",
-          DateTime.now().millisecondsSinceEpoch);
+      await LoginScreenState.flutterXmpp.sendGroupMessage(groupName,
+          _msgController.text, "$id", DateTime.now().millisecondsSinceEpoch);
 
       incomingGrpMsgList
           .add(MessageChat(body: _msgController.text, type: "sender"));
@@ -48,7 +45,7 @@ class _GroupChatScreenState extends State<GroupChatScreen>
 
       setState(() {});
     }
-  }*/
+  }
 
   static const snackBar = SnackBar(
     content: Text('Invite has been sent.'),
@@ -81,7 +78,7 @@ class _GroupChatScreenState extends State<GroupChatScreen>
                     return Colors.grey;
                   }),
                   textStyle: MaterialStateProperty.resolveWith((states) {
-                    return TextStyle(color: Colors.white);
+                    return const TextStyle(color: Colors.white);
                   }),
                 ),
                 child: const Text('CANCEL'),
@@ -98,7 +95,7 @@ class _GroupChatScreenState extends State<GroupChatScreen>
                     return Colors.lightBlue;
                   }),
                   textStyle: MaterialStateProperty.resolveWith((states) {
-                    return TextStyle(color: Colors.white);
+                    return const TextStyle(color: Colors.white);
                   }),
                 ),
                 child: const Text('Add'),
@@ -133,157 +130,139 @@ class _GroupChatScreenState extends State<GroupChatScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        flexibleSpace: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.only(right: 16),
-            child: Row(
-              children: <Widget>[
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(
-                  width: 2,
-                ),
-                const CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://media.istockphoto.com/photos/multi-ethnic-guys-and-girls-taking-selfie-outdoors-with-backlight-picture-id1368965646?b=1&k=20&m=1368965646&s=170667a&w=0&h=9DO-7OKgwO8q7pzwNIb3aq2urlw3DNTmpKQyvvNDWgY="),
-                  maxRadius: 20,
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        widget.groupName,
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600),
+        appBar: AppBar(
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            flexibleSpace: SafeArea(
+                child: Container(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: Row(children: <Widget>[
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.black,
+                        ),
                       ),
-                      const SizedBox(height: 6),
-                      Text(
-                        "total members: 5",
-                        style: TextStyle(
-                            color: Colors.grey.shade600, fontSize: 13),
+                      const SizedBox(
+                        width: 2,
                       ),
-                    ],
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    _displayTextInputDialog(context);
-                  },
-                  icon: const Icon(
-                    Icons.person_add_alt_1_rounded,
-                  ),
-                  color: Colors.black54,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      body: Stack(
-        children: <Widget>[
+                      const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://media.istockphoto.com/photos/multi-ethnic-guys-and-girls-taking-selfie-outdoors-with-backlight-picture-id1368965646?b=1&k=20&m=1368965646&s=170667a&w=0&h=9DO-7OKgwO8q7pzwNIb3aq2urlw3DNTmpKQyvvNDWgY="),
+                        maxRadius: 20,
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              widget.groupName,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w600),
+                            ),
+                            const SizedBox(height: 6),
+                            Text(
+                              "total members: 5",
+                              style: TextStyle(
+                                  color: Colors.grey.shade600, fontSize: 13),
+                            ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _displayTextInputDialog(context);
+                        },
+                        icon: const Icon(
+                          Icons.person_add_alt_1_rounded,
+                        ),
+                        color: Colors.black54,
+                      )
+                    ])))),
+        body: Stack(children: <Widget>[
           ListView.builder(
-            itemCount: incomingGrpMsgList.length,
-            shrinkWrap: true,
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.only(
-                    left: 14, right: 14, top: 10, bottom: 10),
-                child: Align(
-                  alignment: (incomingGrpMsgList[index].type == "sender"
-                      ? Alignment.topRight
-                      : Alignment.topLeft),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: (incomingGrpMsgList[index].type == "sender"
-                          ? Colors.blue[200]
-                          : Colors.grey.shade200),
-                    ),
-                    padding: const EdgeInsets.all(16),
-                    child: Text(
-                      incomingGrpMsgList[index].time ?? '',
-                      style: const TextStyle(fontSize: 15),
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
+              itemCount: incomingGrpMsgList.length,
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Container(
+                    padding: const EdgeInsets.only(
+                        left: 14, right: 14, top: 10, bottom: 10),
+                    child: Align(
+                        alignment: (incomingGrpMsgList[index].type == "sender"
+                            ? Alignment.topRight
+                            : Alignment.topLeft),
+                        child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: (incomingGrpMsgList[index].type == "sender"
+                                  ? Colors.blue[200]
+                                  : Colors.grey.shade200),
+                            ),
+                            padding: const EdgeInsets.all(16),
+                            child: Text(
+                              incomingGrpMsgList[index].body ?? '',
+                              style: const TextStyle(fontSize: 15),
+                            ))));
+              }),
           Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
-              height: 60,
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlue,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 20,
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                  padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Row(children: <Widget>[
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 20,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: TextField(
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                        child: TextField(
                       controller: _msgController,
                       decoration: const InputDecoration(
                           hintText: "Write message...",
                           hintStyle: TextStyle(color: Colors.black54),
                           border: InputBorder.none),
+                    )),
+                    const SizedBox(
+                      width: 15,
                     ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () => {},//sendMsg(),
-                    backgroundColor: Colors.blue,
-                    elevation: 0,
-                    child: const Icon(
-                      Icons.send,
-                      color: Colors.white,
-                      size: 18,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+                    FloatingActionButton(
+                        onPressed: () => sendMsg(),
+                        backgroundColor: Colors.blue,
+                        elevation: 0,
+                        child: const Icon(
+                          Icons.send,
+                          color: Colors.white,
+                          size: 18,
+                        ))
+                  ])))
+        ]));
   }
 
   @override
